3b62e6ef328e1bc254b06f0e8fd4c2de8f1a985a
Michael <ironbee2002@yandex.ru>
2022-02-24 20:23:28 +0700

    Zadacha 6

--- skr.js
+++ skr.js
@@ -1,10 +1,24 @@
 let arr = []
+
 function generate() {
 	let n = parseInt(document.getElementById('age').value)
 for (let i=0; i<n; i++) {
 	arr[i]=Math.floor(Math.random()*10)
 }
-let div = document.getElementById('mes')
+print()}
+
+function zerof() {
+	let n = parseInt(document.getElementById('age').value)
+for (let i=0; i<n; i++) {
+	arr[i]=0
+}
+print()
+}
+
+function print() {
+	let n = parseInt(document.getElementById('age').value)
+	let div = document.getElementById('mes')
+	div.innerHTML = ' '
 for (let i=0;i<n; i++) {
 	div.innerHTML += arr[i] + ' '
 	}
