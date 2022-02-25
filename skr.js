fd255ce41df433f86703b23b87646a53ad6a0507
Michael <ironbee2002@yandex.ru>
2022-02-25 12:44:38 +0700

    Пример 3

--- skr.js
+++ skr.js
@@ -5,6 +5,14 @@ let students = [
 {'id':4, 'name':'Vladimir','surname':'Burikin'},
 {'id':5, 'name':'Semen','surname':'Veligjanin'}
 ]
-for (let i=0; i<students.length; i++) {
-	console.log(students[i].surname+' '+students[i].name)
+let current = 0
+
+function load_student(id) {
+	document.getElementById('name').value = students[id].name
+	document.getElementById('surname').value = students[id].surname
+}
+
+function next() {
+	load_student(current)
+	current++
 }
