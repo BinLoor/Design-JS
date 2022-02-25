c680348241d3a60ddd6597817c9d3e648bddf318
Michael <ironbee2002@yandex.ru>
2022-02-25 12:17:08 +0700

    Пример 1

--- skr.js
+++ skr.js
@@ -1,25 +1,6 @@
-let arr = []
-
-function generate() {
-	let n = parseInt(document.getElementById('age').value)
-for (let i=0; i<n; i++) {
-	arr[i]=Math.floor(Math.random()*10)
+let x = {
+	'name':'Rustam', 
+	'age':18,
+	'height':160.5
 }
-print()}
-
-function zerof() {
-	let n = parseInt(document.getElementById('age').value)
-for (let i=0; i<n; i++) {
-	arr[i]=0
-}
-print()
-}
-
-function print() {
-	let n = parseInt(document.getElementById('age').value)
-	let div = document.getElementById('mes')
-	div.innerHTML = ' '
-for (let i=0;i<n; i++) {
-	div.innerHTML += arr[i] + ' '
-	}
-}
+console.log(x.name)
