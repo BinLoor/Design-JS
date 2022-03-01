2f70a148fa54fc0d091fa32d8d6443048c6ccc4e
Michael <ironbee2002@yandex.ru>
2022-03-01 20:59:28 +0700

    Пример 3 v3 Кнопки далее-назад

--- skr.js
+++ skr.js
@@ -13,11 +13,22 @@ function load_student(id) {
 }
 
 function next() {
-if (current < students.length) {
-	load_student(current)
+	document.getElementById('btnprev').disabled = false
 	current++
+	load_student(current)
+	
+	if (current == 4) {
+		document.getElementById('btnnext').disabled = true
+	}
 }
-else{
-	document.getElementById('btnnext').disabled = true
+
+function prev() {
+	document.getElementById('btnnext').disabled = false
+	current--
+	load_student(current)
+	
+	if (current == 0) {
+		document.getElementById('btnprev').disabled = true
+	}
 }
-}
+
