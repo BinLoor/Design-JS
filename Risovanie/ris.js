16ddc422707383bf12586736b50132604a29bd14
Michael <ironbee2002@yandex.ru>
2022-03-09 12:26:56 +0700

    Кривая Безье

--- Risovanie/ris.js
+++ Risovanie/ris.js
@@ -2,10 +2,10 @@ function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
 
-ctx.beginPath(); // начало нового пути
+ctx.beginPath();// начало нового пути
 ctx.lineWidth = 7; // толщина обводки
 ctx.strokeStyle="green"; // цвет обводки
-ctx.moveTo(75, 75); // координаты начальной точки
-ctx.quadraticCurveTo(100, 150, 300, 50); // координаты точки искривления и конечной точки
-ctx.stroke(); // команда обводки
+ctx.moveTo(100, 150); // координаты начальной точки
+ctx.bezierCurveTo(150, 50, 340, 10, 400, 200); // координаты точки искривления и конечной точки
+ctx.stroke(); 
 }
