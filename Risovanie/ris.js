5185b9732b501c95ef509671a3f739cd5804438f
Michael <ironbee2002@yandex.ru>
2022-03-09 11:54:11 +0700

    Dve Raznie Linii

--- Risovanie/ris.js
+++ Risovanie/ris.js
@@ -1,12 +1,15 @@
 function draw() {
 	let canva = document.getElementById('canva')
 	let ctx = canva.getContext('2d')
-	// координаты начала линии X,Y
-ctx.moveTo(200,100);
+	ctx.moveTo(200,100);
 // команда рисования линии с координатами конца линии
 ctx.lineTo(500,200);
 ctx.strokeStyle = "pink"; //цвет линии
 ctx.lineWidth = "10"; //толщина линии
- ctx.lineTo(500,50);
-ctx.stroke(); // обводка линии     
+ctx.stroke(); // обводка линии
+
+// координаты начала второй линии X,Y
+ctx.moveTo(300,100);
+ctx.lineTo(500,50);
+ctx.stroke();    
 }
