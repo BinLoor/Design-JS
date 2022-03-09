e18053286e89f472791adfff08f06ebaabd4a2b5
Michael <ironbee2002@yandex.ru>
2022-03-09 12:18:25 +0700

    Наложение окружности

--- Risovanie/ris.js
+++ Risovanie/ris.js
@@ -3,11 +3,20 @@ function draw() {
 	let ctx = canva.getContext('2d')
  let pi = Math.PI;
 
- ctx.beginPath();
+  ctx.beginPath();
 ctx.lineWidth = 7;
 ctx.strokeStyle = "green";
-ctx.fillStyle="yellow";
+ctx.fillStyle="red";
 ctx.arc(150, 150, 85, 0, 2*pi, false);
+ctx.stroke();
+ctx.fill();
+
+
+ctx.beginPath();
+ctx.lineWidth = 7;
+ctx.strokeStyle = "gray";
+ctx.fillStyle="orange";
+ctx.arc(250, 150, 85, 0, 2*pi, false );
 ctx.stroke();
 ctx.fill(); 
 }
